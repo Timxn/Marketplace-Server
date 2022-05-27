f46e549ac9b3d6ea9ef76ddf8f4ba280a4cfabc0
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-05-27 13:48:44 +0200

    definition of the functions needed to properly implement the REST-API

---
+++ src/main/java/server/interfaces/Market.java
@@ -0,0 +1,61 @@
+package server.interfaces;
+
+
+import java.util.UUID;
+
+/**
+ * The interface Market.
+ */
+public interface Market {
+    // product management-------------------------------------------------------------
+
+    /**
+     * Get all products
+     *
+     * @implNote GET
+     * @return the object[][]={ {productID, productName, price, avaibleUnits}, ... }
+     */
+    Object[][] getAllProducts();
+
+    /**
+     * Add product uuid. Creates a new product entry if no product with the same name exists, returns the productID in both cases
+     *
+     * @implNote POST
+     * @param productName the product name
+     * @return the productID
+     */
+    UUID addProduct(String productName);
+
+    /**
+     * deletes the given product as identified per its productID
+     *
+     * @implNote DELETE
+     * @param productID the productID of the product that will be deleted
+     */
+    void deleteProductEntry(UUID productID);
+
+    // trading -----------------------------------------------------------------------
+
+    /**
+     * Sell.
+     *
+     * @implNote POST
+     * @throws java.util.NoSuchElementException if user has no such product
+     * @param userID  the userID
+     * @param productID the productID
+     */
+    void sell(UUID userID, UUID productID);
+
+    /**
+     * Buy.
+     *
+     * @implNote POST
+     * @throws java.util.MissingResourceException if user
+     * @param userID   the userID
+     * @param productID the productID
+     * @return the price
+     */
+    double buy(UUID userID, UUID productID);
+
+
+}
