e303737afbb376bfc326c6505094c0a777aea011
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-05-28 00:12:55 +0200

    implementation of authentication check, register, login, logout as REST ENDPOINTS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.google.gson.JsonParser;
 import server.implementation.Users;
 
 import javax.management.InstanceAlreadyExistsException;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static spark.Spark.*;
@@ -11,31 +12,54 @@ public class Main {
     private static Users users = new Users();
     // http://localhost:4567/hello
     public static void main(String[] args) {
-        post("/register", (request, response) -> {
-            System.out.println("DEBUG");
+        before(((request, response) -> {
             JsonObject requestJSON = new JsonParser().parse(request.body()).getAsJsonObject();
-            System.out.println(requestJSON.get("mail"));
-            String username = requestJSON.get("mail").getAsString();
-            response.type("text/json");
-            try {
-                users.register(requestJSON.get("mail").getAsString(), requestJSON.get("password").getAsString());
-            } catch (InstanceAlreadyExistsException e) {
-                return "ERROR";
+            if (!(request.pathInfo().equals("/user/register")) && !(request.pathInfo().equals("/user/login"))) {
+                if ((users.checkToken(UUID.fromString(requestJSON.get("token").toString().replace("\"","")))).equals(null)) halt(401, "Unauthorized");
             }
-                /*
-                curl -X POST http://localhost:4567/register -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d '{"username" : "dieter"}'
-                 */
-            return "TEST";
-        });
+        }));
         path("/user", () -> {
-            post("/login", ((request, response) -> {
-                response.type("text/json");
-                JsonObject temp = new JsonObject();
-                temp.addProperty("alter", 42);
-                temp.addProperty("name", "dieter");
-                return temp;
-            }));
+            post("/register", (request, response) -> {
+                JsonObject requestJSON = new JsonParser().parse(request.body()).getAsJsonObject();
+                String username = requestJSON.get("mail").getAsString();
+                try {
+                    users.register(requestJSON.get("mail").getAsString(), requestJSON.get("password").getAsString());
+                    response.status(201);
+                } catch (InstanceAlreadyExistsException e) {
+                    response.status(405);
+                }
+                return response.status();
+            });
+            post("/login", (request, response) -> {
+                JsonObject requestJSON = new JsonParser().parse(request.body()).getAsJsonObject();
+                UUID token;
+                try {
+                    token = users.login(requestJSON.get("mail").getAsString(), requestJSON.get("password").getAsString());
+                    response.status(201);
+                    JsonObject returnElement = new JsonObject();
+                    returnElement.addProperty("token", token.toString());
+                    return returnElement.toString();
+                } catch (NoSuchElementException e) {
+                    response.status(405);
+                    return response.status();
+                }
+            });
+            delete("/logout", (request, response) -> {
+                JsonObject requestJSON = new JsonParser().parse(request.body()).getAsJsonObject();
+                users.logout(UUID.fromString((requestJSON.get("token").toString()).replace("\"","")));
+                response.status(200);
+                return response.status();
+            });
         });
+//        path("/user", () -> {
+//            post("/login", ((request, response) -> {
+//                response.type("text/json");
+//                JsonObject temp = new JsonObject();
+//                temp.addProperty("alter", 42);
+//                temp.addProperty("name", "dieter");
+//                return temp;
+//            }));
+//        });
 
     }
 }
