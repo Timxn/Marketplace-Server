6f9b157c4a0e43e5d8c4e1ce568d8fa79b14f188
Timon <stronczek.timon@gmail.com>
2022-06-12 23:02:57 +0200

    Added documentation

--- src/main/java/server/implementation/User.java
+++ src/main/java/server/implementation/User.java
@@ -17,6 +17,12 @@ public class User {
         this.balance = 100;
     }
 
+    /**
+     * Adjusts the number of products in the depot.
+     * @param product which product will be changed
+     * @param count the number to be changed (positive or negative)
+     * @throws Exception throws exception if less than 0 products would be available after deduction
+     */
     public void updateDepot(String product, int count) throws Exception {
         if (depot.getOrDefault(product, 0) + count < 0) throw new Exception("Not enough products available!");
         if(depot.containsKey(product)) {
@@ -26,26 +32,50 @@ public class User {
         }
     }
 
+    /**
+     * Returns the userID of the user.
+     * @return userID
+     */
     public UUID getUserID() {
         return userID;
     }
 
+    /**
+     * Returns the mail of the user.
+     * @return mail
+     */
     public String getMail() {
         return mail;
     }
 
+    /**
+     * Returns the password of the user.
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns the amount of money the user has.
+     * @return balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Alters the amount of money the user has.
+     * @param balance the new amount of money
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Returns the depot which all items the user has.
+     * @return depot of the user
+     */
     public HashMap<String, Integer> getDepot() {
         return depot;
     }
