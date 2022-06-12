621073fe6e9a4d8458117f2b3433cdfafa616a51
Timon <stronczek.timon@gmail.com>
2022-06-13 00:45:39 +0200

    Added setBalance() which allows to alter the balance for a user nad added documentation.

--- src/main/java/server/implementation/ShopManager.java
+++ src/main/java/server/implementation/ShopManager.java
@@ -11,6 +11,12 @@ public class ShopManager {
     HashMap<UUID, UUID> allTokens = new HashMap<>();
     Market market = new Market();
 
+    /**
+     * Registers a new user with a mail and a password.
+     * @param mail mail for the new user
+     * @param password password for the new user
+     * @throws InstanceAlreadyExistsException throws exception if a user with this mail is already registered
+     */
     public void register(String mail, String password) throws InstanceAlreadyExistsException { //exception work?
         for (User user : registeredUsers) {
             if (user.getMail().equals(mail)) throw new InstanceAlreadyExistsException("Mail already registered!");
@@ -18,6 +24,12 @@ public class ShopManager {
         registeredUsers.add(new User(mail, password));
     }
 
+    /**
+     * Logs in a user with his passowrt and mail and returns a token.
+     * @param mail mail of the user
+     * @param password password of the user
+     * @return token which the user uses to perform further actions
+     */
     public UUID login(String mail, String password) {
         for (User user : registeredUsers) {
             if (user.getMail().equals(mail) && user.getPassword().equals(password)) {
@@ -29,10 +41,18 @@ public class ShopManager {
         throw new NoSuchElementException("Login failed. Mail or password incorrect!");
     }
 
+    /**
+     * Logs out the user and deletes the token.
+     * @param token which user wants to logout
+     */
     public void logout(UUID token) {
         allTokens.remove(token);
     }
 
+    /**
+     * Deletes an account and makes all its products available on the market.
+     * @param token token with which the account to be deleted is determined
+     */
     public void deleteAccount(UUID token) { //deletes a user and first sells all their products
         User user = getUserByUserID(getUserIDByToken(token));
         HashMap<String, Integer> toSell = new HashMap<>(user.getDepot());
@@ -45,6 +65,13 @@ public class ShopManager {
         logout(token);
     }
 
+    /**
+     * Buys a product from the market adds it to the depot and adjusts the balance of the user and the price of all products.
+     * @param product product the user wants to buy
+     * @param count amount of the product
+     * @param token token of the user
+     * @throws Exception throws exception if the user has to less money for this purchase or there are not enough products on the market
+     */
     public void buyProduct(String product, int count, UUID token) throws Exception {
         UUID userID = getUserIDByToken(token);
         int index = getIndexOfUserByToken(token);
@@ -56,6 +83,13 @@ public class ShopManager {
         market.updatePrice();
     }
 
+    /**
+     * Sells a product from the depot adds it to the market and adjusts the balance of the user and the price of all products.
+     * @param product product the user wants to sell
+     * @param count amount of the product
+     * @param token token of the user
+     * @throws Exception throws exception if the user not enough units of the product he wants to sell
+     */
     public void sellProduct(String product, int count, UUID token) throws Exception {
         UUID userID = getUserIDByToken(token);
         int index = getIndexOfUserByToken(token);
@@ -66,22 +100,54 @@ public class ShopManager {
         market.updatePrice();
     }
 
+    /**
+     * Returns the number of times a product is offered for all products.
+     * @return A Hashmap which contains the names and amounts of all products
+     */
     public HashMap<String, Integer> getAllOffers(){
         return market.getOffers();
     }
 
+    /**
+     * Returns the prices for all products.
+     * @return A Hashmap which contains the names and prices of all products
+     */
     public HashMap<String, Double> getAllPrices() {
         return market.getPrices();
     }
 
+    /**
+     * Returns the depot which all items the user has.
+     * @param token token of the user
+     * @return depot of the user
+     */
     public HashMap<String, Integer> getDepot(UUID token) {
         return registeredUsers.get(getIndexOfUserByToken(token)).getDepot();
     }
 
+    /**
+     * Returns the amount of money the user has.
+     * @param token token of the user
+     * @return balance
+     */
     public double getBalance(UUID token) {
         return registeredUsers.get(getIndexOfUserByToken(token)).getBalance();
     }
 
+    /**
+     * Sets the amount of money for a user.
+     * @param token token of the user
+     * @param amount  amount of money the user gets
+     */
+    public void setBalance(UUID token, double amount) {
+        registeredUsers.get(getIndexOfUserByToken(token)).setBalance(amount);
+    }
+
+    /**
+     * Adds products to the market.
+     * @param product type of the product
+     * @param count amount which will be added
+     */
     public void addProductToOffers(String product, int count) {
         market.sell(product, count);
     }
