345d074b2adb490a9587042d8bbc30926b9e6ee9
Timon <stronczek.timon@gmail.com>
2022-06-12 22:32:35 +0200

    Added Tests

---
+++ src/main/java/server/implementation/tests/MarketTest.java
@@ -0,0 +1,65 @@
+package server.implementation.tests;
+
+import org.junit.jupiter.api.Test;
+import server.implementation.Market;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketTest {
+    Market market = new Market();
+
+    @Test
+    void sell() {
+        assertEquals(150, market.sell("apfel", 3));
+    }
+
+    @Test
+    void buy() throws Exception {
+        assertThrows(Exception.class, () -> market.buy("apfel" , 1));
+        market.sell("apfel", 3);
+        assertEquals(150, market.buy("apfel", 3));
+    }
+
+    @Test
+    void getOffer() {
+        market.sell("apfel", 3);
+        assertEquals(3, market.getOffer("apfel"));
+        assertEquals(0, market.getOffer("bapfel"));
+    }
+
+    @Test
+    void getOffers() {
+        market.sell("apfel", 3);
+        market.sell("bapfel", 2);
+        HashMap<String, Integer> offers = new HashMap<>(market.getOffers());
+        assertEquals(3, offers.get("apfel"));
+        assertEquals(2, offers.get("bapfel"));
+    }
+
+    @Test
+    void getPrice() {
+        market.sell("apfel", 3);
+        assertEquals(50, market.getPrice("apfel"));
+        assertEquals(0, market.getPrice("bapfel"));
+    }
+
+    @Test
+    void getPrices() {
+        market.sell("apfel", 3);
+        market.sell("bapfel", 3);
+        HashMap<String, Double> prices = new HashMap<>(market.getPrices());
+        assertEquals(50, prices.get("apfel"));
+        assertEquals(50, prices.get("bapfel"));
+    }
+
+    @Test
+    void updatePrice() {
+        market.sell("apfel", 3);
+        market.sell("bapfel", 7);
+        market.updatePrice();
+        assertEquals(83.33333333333334, market.getPrice("apfel"));
+        assertEquals(35.714285714285715, market.getPrice("bapfel"));
+    }
+}
---
+++ src/main/java/server/implementation/tests/ShopManagerTest.java
@@ -0,0 +1,99 @@
+package server.implementation.tests;
+
+import org.junit.jupiter.api.Test;
+import server.implementation.ShopManager;
+
+import javax.management.InstanceAlreadyExistsException;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopManagerTest {
+    ShopManager shopManager = new ShopManager();
+
+    @Test
+    void register() throws InstanceAlreadyExistsException {
+        shopManager.register("123", "123");
+        assertThrows(InstanceAlreadyExistsException.class, () -> shopManager.register("123", "123"));
+    }
+
+    @Test
+    void login() throws InstanceAlreadyExistsException {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        assertThrows(NoSuchElementException.class, () -> shopManager.login("13", "123"));
+    }
+
+    @Test
+    void logout() throws InstanceAlreadyExistsException {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        shopManager.logout(token);
+    }
+
+    @Test
+    void buyProduct() throws Exception {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        shopManager.addProductToOffers("apfel", 4);
+        shopManager.buyProduct("apfel", 2, token);
+        assertEquals(0.0, shopManager.getBalance(token));
+        assertEquals(2, shopManager.getDepot(token).get("apfel"));
+        assertEquals(2, shopManager.getAllOffers().get("apfel"));
+    }
+
+    @Test
+    void sellProduct() throws Exception {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        shopManager.addProductToOffers("apfel", 4);
+        shopManager.buyProduct("apfel", 2, token);
+        shopManager.sellProduct("apfel", 1, token);
+        assertEquals(125.0, shopManager.getBalance(token));
+        assertEquals(1, shopManager.getDepot(token).get("apfel"));
+        assertEquals(3, shopManager.getAllOffers().get("apfel"));
+    }
+
+    @Test
+    void getAllOffers() {
+        shopManager.addProductToOffers("apfel", 4);
+        assertEquals(4, shopManager.getAllOffers().get("apfel"));
+    }
+
+    @Test
+    void getAllPrices() {
+        shopManager.addProductToOffers("apfel", 4);
+        assertEquals(50.0, shopManager.getAllPrices().get("apfel"));
+    }
+
+    @Test
+    void getDepot() throws Exception {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        shopManager.addProductToOffers("apfel", 4);
+        shopManager.buyProduct("apfel", 2, token);
+        assertEquals(2, shopManager.getDepot(token).get("apfel"));
+    }
+
+    @Test
+    void getBalance() throws Exception {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        shopManager.addProductToOffers("apfel", 4);
+        shopManager.buyProduct("apfel", 1, token);
+        assertEquals(50.0, shopManager.getBalance(token));
+    }
+
+    @Test
+    void deleteAccount() throws Exception {
+        shopManager.register("123", "123");
+        UUID token = shopManager.login("123", "123");
+        shopManager.addProductToOffers("apfel", 4);
+        shopManager.buyProduct("apfel", 2, token);
+        assertEquals(2, shopManager.getAllOffers().get("apfel"));
+        shopManager.deleteAccount(token);
+        assertEquals(4, shopManager.getAllOffers().get("apfel"));
+    }
+}
